0d8c2bbc40c13649db22ad4cad484e36ccd9aaa8
syawalqi <galihmaulanasyawalqi@gmail.com>
2024-03-26 15:00:12 +0700

    membuat switch

--- switch.cpp
+++ switch.cpp
@@ -41,5 +41,31 @@ int main(){
         cout << "====================" << endl;
         cout << "Masukan menu pilihan" << endl;
         cin >> pilihan;
+
+
+        switch (pilihan){
+            case 1:
+            inputdata();
+            cout << "hasil penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
+            break;
+            case 2:
+            inputdata();
+            cout << "hasil pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
+            break;
+            case 3:
+            inputdata();
+            cout << "hasil perkalian = " << perkalian(bilangan1, bilangan2) << endl;
+            break;
+            case 4:
+            inputdata();
+            cout << "hasil pembagian = " << pembagian(bilangan1, bilangan2) << endl;
+            break;
+            case 5:
+            break;
+
+            default:
+            cout << "pilihan tidak ada" << endl;
+        }
+        
      }
 }
